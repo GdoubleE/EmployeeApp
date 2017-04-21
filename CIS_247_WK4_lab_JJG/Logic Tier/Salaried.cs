50dcff1899ba8e02cbfed8be47bcb00406912941
GdoubleE <jjgee@xactware.com>
2017-04-20 23:45:46 -0600

    Added Employee App

---
+++ CIS_247_WK4_lab_JJG/Logic Tier/Salaried.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Employee
+{
+    class Salaried : Employee
+    {
+        #region Attributes
+        private const int MIN_MANAGEMENT_LEVEL = 0;
+        private const int MAX_MANAGEMENT_LEVEL = 3;
+        private const double BONUS_PERCENT = .10;
+        private const double TAX_RATE = .73;
+
+        private int managementLevel;
+        #endregion
+
+        #region Constructors
+        public Salaried() :base("Salaried")
+        {
+        }
+
+        public Salaried(string employeeType, string firstName, string lastName, char gender, int dependents, double annualSalary, Benefits benefits, int managementLevel,double netPay)
+            : base("Salaried", firstName, lastName, gender, dependents, annualSalary, benefits)
+                {
+                    managementLevel = ManagementLevel;
+                }
+        #endregion
+
+        #region Properties
+
+        public int ManagementLevel
+        {
+            get
+                {return managementLevel;}
+            set
+                {
+                    if (value <= MIN_MANAGEMENT_LEVEL)
+                        {
+                            managementLevel = MIN_MANAGEMENT_LEVEL;
+                        }
+                    else if (value >= MAX_MANAGEMENT_LEVEL)
+                        {
+                            managementLevel = MAX_MANAGEMENT_LEVEL;
+                        }
+                    else
+                        {
+                            managementLevel = value;
+                        }
+                }
+        }
+        #endregion
+
+        #region Calculations
+        public double ActualBonusPercent()
+            {
+                return ManagementLevel * BONUS_PERCENT;
+            }
+
+        public override double CalculateWeeklyPay()
+            {
+                return (annualSalary*(1+ActualBonusPercent()))/52;
+            }
+
+        public override double CalculateNetPay()
+            {
+                return CalculateWeeklyPay() * TAX_RATE;
+            }
+
+        public override string ToString()
+            {
+                string output;
+                output = base.ToString();
+                output += "\n             Management Level:                     " + ManagementLevel;
+                output += "\n             Percent Bonus:                        " + ActualBonusPercent().ToString("p1");
+                output += benefit.ToString();
+                return output;
+            }
+        #endregion
+
+
+    }
+    }
+
---
+++ CIS_247_WK4_lab_JJG/Presentation Tier/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //display system header
+            ApplicationUtilities.DisplayApplicationInformation();
+
+            // create an employee array with 4 positions to hold Employee information
+            Employee[] employees = new Employee[4];
+            
+            ApplicationUtilities.DisplayDivider("Employee Type");  //call for employee type
+
+            FillEmployeeArray(employees);  
+            
+            EmployeeOutput.DisplayNumberObject();
+     
+
+            ApplicationUtilities.PauseExecution();
+
+                Console.ReadLine();
+
+            
+        }
+
+        private static void FillEmployeeArray(Employee[] employees)
+        {
+            
+            int count = 0;
+            do
+            {
+                int typeIn = InputUtilities.getIntegerInputValue("Employee Type:    1:  for Salaried    2: for Hourly:    ");
+                if (typeIn == 1)
+                {
+                    employees[count] = new Salaried();
+                    ApplicationUtilities.DisplayDivider("Enter the Employee Information");
+                    EmployeeInput.CollectSalariedInformation((Salaried)employees[count]);
+                    EmployeeOutput.DisplayEmployeeInformation((Salaried)employees[count]);
+                    ApplicationUtilities.DisplayDivider("Enter Next Employees information");
+                    count++;  //Increment to next Index
+                }
+                else if (typeIn == 2)
+                {
+                    employees[count] = new Hourly();
+                    ApplicationUtilities.DisplayDivider("\n============ Enter the Employee Information ================\n");
+                    EmployeeInput.CollectHourlyInformation((Hourly)employees[count]);
+                    EmployeeOutput.DisplayEmployeeInformation((Hourly)employees[count]);
+                    ApplicationUtilities.DisplayDivider("------Enter Next Employees information----------");
+                    count++;  //Increment to next Index
+                }
+                else
+                {
+                    Console.WriteLine("Thats not a valid Employee Type. Please try again");
+                }
+            } while (count < employees.Length);
+        }
+
+        
+    }
+}
